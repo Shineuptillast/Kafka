9ce41e9c1c6b5de375d8b0f8ffbb376f19647ced
Naman Jain <90482311+Shineuptillast@users.noreply.github.com>
2023-07-03 20:15:51 +0530

    Create Producer File1.py

---
+++ Producer File1.py
@@ -0,0 +1,33 @@
+# loading the necessary libraries
+from confluent_kafka import Producer
+from confluent_kafka.serialization import StringSerializer, IntegerSerializer
+from json import dumps, loads
+import os
+from typing import Dict
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def sasl_config():
+    return {'bootstrap.servers': os.getenv('BOOTSTRAP_SERVER'),
+            'security.protocol': os.getenv('SECURITY_PROTOCOL'),
+            'sasl.mechanisms': os.getenv('SSL_MECHANISM'),
+            'sasl.username': os.getenv('API_KEY'),
+            'sasl.password': os.getenv('API_SECRET_KEY')
+            }
+
+
+def main():
+    my_topic='topic_0'
+    int_serializer = IntegerSerializer()
+    producer = Producer(sasl_config())
+    for i in range(100):
+        print(i)
+        producer.produce(topic=my_topic, value=int_serializer(i))
+    producer.flush()
+
+main()
+
+
+
