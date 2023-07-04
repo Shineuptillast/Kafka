7af3337733cf861a946604fef6e3f1959ed5db1b
Naman Jain <90482311+Shineuptillast@users.noreply.github.com>
2023-07-05 00:02:32 +0530

    Update Producer File 3.py
    JSON Data Serializer

--- Producer File 3.py
+++ Producer File 3.py
@@ -1,38 +1,33 @@
-from confluent_kafka import Consumer
-from confluent_kafka.serialization import IntegerDeserializer, StringDeserializer
+# loading the necessary libraries
+from confluent_kafka import Producer
+from confluent_kafka.serialization import StringSerializer, IntegerSerializer
+from json import dumps, loads
 import os
 from dotenv import load_dotenv
-from json import loads
 
 load_dotenv()
 
 
 def sasl_config():
-    return {
-        'sasl.mechanisms': os.getenv("SSL_MECHANISM"),
-        'bootstrap.servers': os.getenv('BOOTSTRAP_SERVER'),
-        'security.protocol': os.getenv("SECURITY_PROTOCOL"),
-        'sasl.username': os.getenv("API_KEY"),
-        'sasl.password': os.getenv("API_SECRET_KEY"),
-        'group.id': os.getenv('GROUP_ID'),
-        'auto.offset.reset': os.getenv("AUTO_OFFSET_RESET")
-
-    }
+    return {'bootstrap.servers': os.getenv('BOOTSTRAP_SERVER'),
+            'security.protocol': os.getenv('SECURITY_PROTOCOL'),
+            'sasl.mechanisms': os.getenv('SSL_MECHANISM'),
+            'sasl.username': os.getenv('API_KEY'),
+            'sasl.password': os.getenv('API_SECRET_KEY')
+            }
 
 
 def main():
-    my_topic = "topic_0"
-    consumer = Consumer(sasl_config())
-    consumer.subscribe([my_topic])
-    strdeserial = StringDeserializer(codec='utf-8')
-    while True:
-        msg = consumer.poll(1.0)
-
-        if msg is None:
-            continue
-        value = loads(strdeserial(msg.value()))
-        print(value, msg.offset(), msg.partition())
-    consumer.close()
-
-
+    my_topic="topic_0"
+    string_serializer = StringSerializer(codec='utf-8')
+    producer = Producer(sasl_config())
+    for i in range(1,500):
+        data = {'data': i}
+        producer.produce(topic=my_topic, value=string_serializer(dumps(data)))
+        print("Items in a Queue", len(producer))
+        producer.poll(1)
+    producer.flush()
 main()
+
+
+
